b33fe31dce1b374f5b9237ccea30fee88c723cd6
mai cook <mailiecook@yahoo.com>
2020-03-24 11:33:37 -0700

    add functions .click to scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+});
+$("button#pink").click(funtion() {
+  $("body").addClass("pink-background")
+});
+$("button#blue").click(function() {
+  $("body").addClass("blue-background");
+  });
+});
